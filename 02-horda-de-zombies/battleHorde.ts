7a9eb12cadebc4c5f0159c7ae8637d1f9000e7a1
S3BAAS <sebastejeda392@gmail.com>
2024-10-31 15:26:30 -0300

    Fix incorrect power calculation in zombie horde challenge

--- 02-horda-de-zombies/battleHorde.ts
+++ 02-horda-de-zombies/battleHorde.ts
@@ -1,25 +1,83 @@
 function battleHorde(zombies: string, humans: string): string {
-  let zombiePower = zombies.split('').map(Number)
-  let humanPower = humans.split('').map(Number)
+  let charZombie = zombies.split('').map(Number)
+  let charHuman = humans.split('').map(Number)
 
-  const numberLastZombie = zombiePower.length - 1
+  let arrayZombie: number[] = []
+  let arrayHuman: number[] = []
 
-  for (let i = 0; i < zombiePower.length; i++) {
-    if (zombiePower[i] > humanPower[i]) {
-      zombiePower[i + 1] = (zombiePower[i + 1 || 0]) + (zombiePower[i] - humanPower[i])
-    } else if (zombiePower[i] < humanPower[i]) {
-      humanPower[i + 1] = (humanPower[i + 1 || 0]) + (humanPower[i] - zombiePower[i])
+  let winner: string = 'z'
+
+  for (let i = 0; i < charZombie.length; i++) {
+    if (charZombie[i] > charHuman[i]) {
+      let resZombie = charZombie[i] - charHuman[i]
+
+      charZombie[i + 1] += resZombie
+      arrayZombie.push(resZombie)
+
+      if (i === charZombie.length - 2) {
+        winner = 'z'
+      }
+    }
+
+    if (charHuman[i] > charZombie[i]) {
+      let resHuman = charHuman[i] - charZombie[i]
+
+      charHuman[i + 1] += resHuman
+      arrayHuman.push(resHuman)
+
+      if (i === charHuman.length - 1) {
+        winner = 'h'
+      }
+    }
+
+    if (charZombie[i] === charHuman[i]) {
+      let res = charZombie[i] - charHuman[i]
+      charZombie[i + 1] += res
+      arrayZombie.push(res)
+
+      charHuman[i + 1] += res
+      arrayHuman.push(res)
     }
   }
 
-  const lastZombie = zombiePower[numberLastZombie] || 0
-  const lastHuman = humanPower[numberLastZombie] || 0
+  const lastZombie = arrayZombie[arrayZombie.length - 1]
+  const lastHuman = arrayHuman[arrayHuman.length - 1]
 
-  if (lastZombie > lastHuman) {
+  const lengthZombie = lastZombie > lastHuman
+  const lengthHuman = lastHuman > lastZombie
+
+  const condZombie = lastZombie !== undefined
+  const condZombie2 = lastZombie > 0
+
+  const condHuman = lastHuman !== undefined
+  const condHuman2 = lastHuman > 0
+
+
+  if (lengthZombie) {
     return `${lastZombie}z`
-  } else if (lastZombie < lastHuman) {
+  }
+
+  if (!arrayHuman.length) {
+    return `${lastZombie}z`
+  }
+
+  if (lengthHuman) {
     return `${lastHuman}h`
   }
+
+  if (!arrayZombie.length) {
+    return `${lastHuman}h`
+  }
+
+
+  if (winner === 'z' && condZombie && condZombie2) {
+    return `${lastZombie}z`
+  }
+
+  if (winner === 'h' && condHuman && condHuman2) {
+    return `${lastHuman}h`
+  }
+
 
   return 'x'
 }
